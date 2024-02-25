4deeee6b9b98dedfa057e599a23f33d90ca9c021
JustAnyone <dom.svetikas@gmail.com>
2024-02-25 12:03:26 +0200

    Use in-built AuthenticationPrincipal annotation for getting current user

--- Receptai-server/src/main/java/lt/tastybytes/receptaiserver/controller/RecipeController.java
+++ Receptai-server/src/main/java/lt/tastybytes/receptaiserver/controller/RecipeController.java
@@ -3,10 +3,9 @@ package lt.tastybytes.receptaiserver.controller;
 import lt.tastybytes.receptaiserver.model.Recipe;
 import lt.tastybytes.receptaiserver.model.User;
 import lt.tastybytes.receptaiserver.service.RecipeService;
-import lt.tastybytes.receptaiserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,14 +20,14 @@ public class RecipeController {
     @PostMapping(path="/create")
     public ResponseEntity createNewRecipe(
             @RequestParam String name,
-            @RequestParam String description
+            @RequestParam String description,
+            @AuthenticationPrincipal User user
     ) {
 
-       var auth = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-       System.out.println(auth);
+
        //if ()
 
-        recipeService.createRecipe(name, description, (User) auth);
+        recipeService.createRecipe(name, description, user);
 
 
 
@@ -44,7 +43,6 @@ public class RecipeController {
 
     @GetMapping("/list")
     public @ResponseBody Iterable<Recipe> getAllRecipes() {
-        // This returns a JSON or XML with the users
         return recipeService.getAllRecipes();
     }
 

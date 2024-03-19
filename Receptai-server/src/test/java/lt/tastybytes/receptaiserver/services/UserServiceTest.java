0a416a04132744ea8cea83d8c082128b41280696
JustAnyone <dom.svetikas@gmail.com>
2024-03-19 19:29:56 +0200

    Write more user service tests

--- Receptai-server/src/test/java/lt/tastybytes/receptaiserver/services/UserServiceTest.java
+++ Receptai-server/src/test/java/lt/tastybytes/receptaiserver/services/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 @Import(TestDatabaseConfig.class)
@@ -30,4 +31,26 @@ public class UserServiceTest {
         var result = userService.findAllUsers();
         assertEquals(1, result.size());
     }
+
+    @Test
+    void newlyCreatedUserShouldHave1Role() throws Exception {
+        userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
+        var user = userService.findAllUsers().get(0);
+        assertEquals(1, user.getRoles().size());
+    }
+
+    @Test
+    void newlyCreatedUserShouldHaveDefaultRoleOfUser() throws Exception {
+        userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
+        var user = userService.findAllUsers().get(0);
+        assertEquals("ROLE_USER", user.getRoles().get(0).getName());
+    }
+
+    @Test
+    void findUserByEmailForWrongEmailShouldReturnNull() throws Exception {
+        userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
+        var user = userService.findUserByEmail("invalid@email.com");
+        assertNull(user);
+    }
+
 }

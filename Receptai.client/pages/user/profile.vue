d198d7fe92812c8302998fcf6866d97ac8ac5900
Erikas Laužadis <elauzadis@gmail.com>
2024-03-22 11:12:53 +0200

    Change profile page to require authorization.

--- Receptai.client/pages/user/profile.vue
+++ Receptai.client/pages/user/profile.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-
+definePageMeta({
+  middleware: "auth",
+});
 </script>
 
 <template>
-    <div>
-        Profile
-    </div>
+  <div>Profile</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>

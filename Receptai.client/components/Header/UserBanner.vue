e417e62c967c19c11d13e2a7eb2e7ff5580ec999
Erikas Laužadis <45915900+ErikasLau@users.noreply.github.com>
2024-03-22 19:41:04 +0200

    Added user profile menu (#8)
    This fulfills REC-34 
    
    * Created a header and a footer, defined default template. Created a recipe container which is currently displayed in the main page.
    
    * Create a dockerfile
    
    * Added a Recipe page with mock data which is shown when Recepie container is pressed. Recipe page has all data that of a recipe.
    
    * Added login page
    
    * Added login and register pages and minimal logic on error handling and submiting forms without API. Changed mock images directory to public.
    
    * Log in/Register API progress - not yet working.
    
    * Update on log in/register API implementation - not yet working.
    
    * Working backend over proxy. Connected register, login forms to API. Added auth logic (saving data to cookie) to dashboard, login, register pages.
    
    * Uppdated login form with better error control.
    
    * Revert some gradle changes
    
    * Remove package files at project root
    
    * Recipes are based on API data except of categories and tags (which are still not implemented in API). Changed images to be more optimized.
    
    * Changed recipes fetching from nuxt useFetch to axios
    
    * Updated code base to use typescript. Some code refactoring, fixing axios errors on development version.
    
    * Added recipe creation page and my recipes page with connection to the API
    
    * Recreated my recipes page with displaying tada in a table. Defined theme colors and addapted them to page.
    
    * Added notification component with logic to website.
    
    * Fixed bugs in Create Recipe page. Added multiple tags selection.
    
    * Turned off ssr. Added validation to login, register.
    
    * Added recipe editing. Some improvments on notification handling. Removed unesacary files.
    
    * Added zod validation to recipe editing and creation.
    
    * Changed docker file for the updated nuxt app structure.
    
    * Updated npm packages. Deleted not used files. Changes notification X icon.
    
    * Change back the formatting of the compose file
    
    * Added and tested testing funcionality to nuxt.
    
    * Minor improvments on header and recipe components to better display on mobile devices. Decomposed edit and create pages components in to seperate components to be easier to read/change.
    
    * Added colapsing to edit, create recipes pages ingredients group and instructions.
    
    * Removed unnecessary texts at the bottom of some files
    
    * Added user profile menu
    
    * Added ability to close user profile menu by pressing outside it. Changed notification to reset before every new notification.
    
    * Fix profile picture not getting size as prop.
    
    * Make notifications longer
    
    * Change profile page to require authorization.
    
    * Extract profile menu to a diffrent component
    
    ---------
    
    Co-authored-by: JustAnyone <dom.svetikas@gmail.com>

--- Receptai.client/components/Header/UserBanner.vue
+++ Receptai.client/components/Header/UserBanner.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import ProfilePicture from "./components/ProfilePicture.vue";
+import ProfileMenu from "./components/ProfileMenu.vue";
+
 import { addNotification } from "@/store/store";
 
 interface User {
@@ -7,38 +10,106 @@ interface User {
   email: string;
 }
 
+interface Navigation {
+  to: string;
+  title: string;
+}
+
 interface UserCookie {
   token: string;
   expiresIn: number;
   user: User;
 }
 
-const user_name = ref("");
-const user_email = ref("");
+const user = ref<User | null>(null);
+const hover = ref(false);
 
 const TastyBytes_user = useCookie<UserCookie | null>("TastyBytes_user");
 
 if (TastyBytes_user.value?.user) {
-  user_name.value = TastyBytes_user.value.user.name;
-  user_email.value = TastyBytes_user.value.user.email;
+  user.value = TastyBytes_user.value.user;
 } else {
   TastyBytes_user.value = null;
 }
 
 const logout = () => {
   TastyBytes_user.value = null;
+  user.value = null;
   navigateTo("/user/login");
+  hover.value = false;
   addNotification(
     "You've been successfully logged out. See you next time!",
     "Success"
   );
 };
+
+const navigation: Array<Navigation> = [
+  {
+    to: "/user/dashboard",
+    title: "Dashboard",
+  },
+  {
+    to: "/user/dashboard/my-recipes",
+    title: "My recipes",
+  },
+  {
+    to: "/user/dashboard/my-recipes/create",
+    title: "Create new recipe",
+  },
+];
+
+const vClickOutside = {
+  mounted(el: any, binding: { value: (event: Event) => void }) {
+    const toggleListener = () => {
+      if (hover.value) {
+        el.__ClickOutsideHandler__ = (event: Event) => {
+          if (!(el === event.target || el.contains(event.target as Node))) {
+            binding.value(event);
+          }
+        };
+        document.body.addEventListener("click", el.__ClickOutsideHandler__);
+      } else {
+        document.body.removeEventListener(
+          "click",
+          el.__ClickOutsideHandler__ as EventListener
+        );
+        el.__ClickOutsideHandler__ = undefined;
+      }
+    };
+
+    toggleListener();
+
+    watch(hover, toggleListener);
+  },
+  unmounted(el: any) {
+    document.body.removeEventListener(
+      "click",
+      el.__ClickOutsideHandler__ as EventListener
+    );
+  },
+};
 </script>
 
 <template>
-  <div v-if="user_name" class="flex flex-col items-end">
-    <NuxtLink to="/user/dashboard" class="font-bold">{{ user_name }}</NuxtLink>
-    <div @click="logout" class="cursor-pointer">Logout</div>
+  <div
+    v-if="user"
+    class="flex flex-col items-end"
+    v-click-outside="() => (hover = false)"
+  >
+    <div class="font-bold" @click="() => (hover = !hover)">
+      <ProfilePicture
+        class="hover:shadow-[0px_0px_0_6px_#00000020] hover:scale-105 transition-shadow duration-150 cursor-pointer w-8 h-8"
+        :class="hover ? 'shadow-[0px_0px_0_6px_#00000010]' : null"
+        :user_name="user.name"
+      />
+    </div>
+    <ProfileMenu
+      v-if="hover"
+      :user
+      :navigation
+      @logout="logout"
+      @page-exit="hover = false"
+    />
   </div>
   <div v-else>
     <NuxtLink to="/user/login">
---
+++ Receptai.client/components/Header/components/ProfilePicture.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  user_name: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="bg-indigo-300 rounded-full flex justify-center items-center font-semibold"
+  >
+    {{ user_name.substring(0, 2) }}
+  </div>
+</template>
+
+<style scoped></style>

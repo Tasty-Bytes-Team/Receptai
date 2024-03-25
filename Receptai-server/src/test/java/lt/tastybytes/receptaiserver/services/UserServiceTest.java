8af27860e6a75122ab9bdc50323774b9fb4316d2
JustAnyone <dom.svetikas@gmail.com>
2024-03-25 12:15:19 +0200

    Clean up some tests and setup coverage

--- Receptai-server/src/test/java/lt/tastybytes/receptaiserver/services/UserServiceTest.java
+++ Receptai-server/src/test/java/lt/tastybytes/receptaiserver/services/UserServiceTest.java
@@ -3,6 +3,7 @@ package lt.tastybytes.receptaiserver.services;
 import lt.tastybytes.receptaiserver.TestDatabaseConfig;
 import lt.tastybytes.receptaiserver.dto.user.PatchUserDto;
 import lt.tastybytes.receptaiserver.exception.NotFoundException;
+import lt.tastybytes.receptaiserver.model.user.User;
 import lt.tastybytes.receptaiserver.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,55 +28,56 @@ public class UserServiceTest {
         userService.createUser("Test User 3", "TestUser3@email.com", "A Super Password 123!");
     }
 
+    User getTestUser1() {
+        return userService.findUserById(1).get();
+    }
+
     @Test
-    void findAllUsers_WhenNoUsersExist_shouldReturn0Elements() throws Exception {
+    void findAllUsers_WhenNoUsersExist_shouldReturn0Elements() {
         var result = userService.findAllUsers();
         assertEquals(0, result.size());
     }
 
     @Test
-    void findAllUsers_When1UserCreated_shouldReturn1Element() throws Exception {
+    void findAllUsers_When1UserCreated_shouldReturn1Element() {
         userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
         var result = userService.findAllUsers();
         assertEquals(1, result.size());
     }
 
     @Test
-    void newlyCreatedUserShouldHave1Role() throws Exception {
+    void findFirstUser_WhenUserNewlyCreated_ShouldHave1Role() {
         userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
         var user = userService.findAllUsers().get(0);
         assertEquals(1, user.getRoles().size());
     }
 
     @Test
-    void newlyCreatedUserShouldHaveDefaultRoleOfUser() throws Exception {
+    void findFirstUser_WhenUserNewlyCreated_ShouldHaveDefaultRoleOfUser() {
         userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
         var user = userService.findAllUsers().get(0);
         assertEquals("ROLE_USER", user.getRoles().get(0).getName());
     }
 
     @Test
-    void findUserByEmailForWrongEmailShouldReturnNull() throws Exception {
+    void findUserByEmail_WhenEmailIsWrong_ShouldReturnNull() {
         userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
         var user = userService.findUserByEmail("invalid@email.com");
         assertNull(user);
     }
 
     @Test
-    void newlyCreatedUserShouldBeOfId1AndShouldReturnCorrectData() throws Exception {
+    void findUserById_WhenUserNewlyCreated_ShouldBeOfId1AndReturnCorrectData() {
         userService.createUser("Test User", "TestUser@email.com", "Very Secret Password 123!");
-        var user = userService.findUserById(1).get();
+        var user = getTestUser1();
         assertEquals(user.getEmail(), "TestUser@email.com");
         assertEquals(user.getName(), "Test User");
     }
 
     @Test
-    void userEdit_ShouldBeAppliedWithValidOldPassword() throws NotFoundException {
+    void userEdit_WithValidOldPassword_ShouldBeApplied() throws NotFoundException {
         createTestUsers();
-        var user = userService.findUserById(1).get();
-
-        assertEquals("TestUser1@email.com", user.getEmail());
-
+        var user = getTestUser1();
         var editedUser = userService.editUser(user, new PatchUserDto(
                 "Naujas Vardas",
                 "new@email.com",
@@ -83,16 +85,14 @@ public class UserServiceTest {
                 "Very Secret Password 123!",
                 "https://media.contentapi.ea.com/content/dam/gin/images/2017/01/command-and-conquer-red-alert-3-key-art.jpg.adapt.crop16x9.575p.jpg"
         ));
-
-
         assertEquals("Naujas Vardas", editedUser.getName());
         assertEquals("new@email.com", editedUser.getEmail());
     }
 
     @Test
-    void userPasswordEdit_ShouldFailWithNoOldPassword() throws NotFoundException {
+    void userPasswordEdit_WithNoOldPassword_ShouldFail() {
         createTestUsers();
-        var user = userService.findUserById(1).get();
+        var user = getTestUser1();
         assertThrows(BadCredentialsException.class, () -> {
             userService.editUser(user, new PatchUserDto(
                     null,
@@ -105,9 +105,9 @@ public class UserServiceTest {
     }
 
     @Test
-    void userEmailEdit_ShouldFailWithNoOldPassword() throws NotFoundException {
+    void userEmailEdit_WithNoOldPassword_ShouldFail() {
         createTestUsers();
-        var user = userService.findUserById(1).get();
+        var user = getTestUser1();
         assertThrows(BadCredentialsException.class, () -> {
             userService.editUser(user, new PatchUserDto(
                     null,
@@ -120,9 +120,9 @@ public class UserServiceTest {
     }
 
     @Test
-    void userPasswordEdit_ShouldFailWithBadOldPassword() throws NotFoundException {
+    void userPasswordEdit_WithBadOldPassword_ShouldFail() {
         createTestUsers();
-        var user = userService.findUserById(1).get();
+        var user = getTestUser1();
         assertThrows(BadCredentialsException.class, () -> {
             userService.editUser(user, new PatchUserDto(
                     null,
@@ -135,15 +135,15 @@ public class UserServiceTest {
     }
 
     @Test
-    void userPasswordEdit_ShouldWorkWithValidOldPassword() throws NotFoundException {
+    void userPasswordEdit_WithValidOldPassword_ShouldSucceed() throws NotFoundException {
         createTestUsers();
-        var user = userService.findUserById(1).get();
+        var user = getTestUser1();
         userService.editUser(user, new PatchUserDto(
-                    null,
-                    null,
-                    "newPassword1",
-                    "Very Secret Password 123!",
-                    null
+                null,
+                null,
+                "newPassword1",
+                "Very Secret Password 123!",
+                null
         ));
     }
 

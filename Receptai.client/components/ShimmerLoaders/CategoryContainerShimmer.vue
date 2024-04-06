b347cb7d82e1b4862dbd4ef564adee41d3729eca
Erikas Laužadis <elauzadis@gmail.com>
2024-04-06 21:15:43 +0300

    Added pusling to the shimmers, created a loader for my recipes

--- Receptai.client/components/ShimmerLoaders/CategoryContainerShimmer.vue
+++ Receptai.client/components/ShimmerLoaders/CategoryContainerShimmer.vue
@@ -5,7 +5,7 @@
     class="lg:basis-1/4 sm:basis-1/3 xsm:basis-1/2 basis-full"
     data-testid="recipe-container"
   >
-    <div class="m-3">
+    <div class="m-3 animate-pulse">
       <div class="m-auto rounded-md lg:h-56 h-48 bg-concrete-300"></div>
       <div class="px-3 py-2 flex flex-col gap-2">
         <div class="flex flex-col gap-1">

1f876d90031681db429d4e6428f252fe29932ea3
Issiins <160136271+Issiins@users.noreply.github.com>
2024-04-09 23:57:31 +0300

    Banner component creation
    Created banner component, even though it lacks features:
    Sorting by recipe popularity or rating

--- Receptai.client/pages/index.vue
+++ Receptai.client/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import RecipeContainerList from "@/components/RecipeContainerList/RecipeContainerList.vue";
+import Banner from "@/components/Banner/Banner.vue";
 </script>
 
 <template>
+  <Banner />
   <RecipeContainerList />
 </template>

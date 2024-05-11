18f82a8488f4dce6e296d008391762abba7c5476
JustAnyone <dom.svetikas@gmail.com>
2024-05-11 10:26:43 +0300

    Allow to not specify sort direction or sort key

--- Receptai-server/src/main/java/lt/tastybytes/receptaiserver/dto/SortedRequestDto.java
+++ Receptai-server/src/main/java/lt/tastybytes/receptaiserver/dto/SortedRequestDto.java
@@ -2,22 +2,35 @@ package lt.tastybytes.receptaiserver.dto;
 
 import lt.tastybytes.receptaiserver.utils.Sorter;
 import org.springframework.data.domain.Sort;
+import org.springframework.lang.Nullable;
 
-public record SortedRequestDto(
-        String sortBy,
-        boolean sortAsc
-) {
+import java.util.Objects;
+
+public class SortedRequestDto {
+
+    @Nullable
+    private final String sortBy;
+    @Nullable
+    private final boolean sortAsc;
+
+    public SortedRequestDto(String sortBy, Boolean sortAsc) {
+        this.sortBy = sortBy;
+        this.sortAsc = Objects.requireNonNullElse(sortAsc, true);
+    }
+
+    @Nullable
+    public String getSortBy() {
+        return sortBy;
+    }
+
     public Sort.Direction getSortDirection() {
         if (sortAsc) return Sort.Direction.ASC;
         return Sort.Direction.DESC;
     }
 
-    public String getSortBy() {
-        return sortBy;
-    }
-
     public Sorter toSorterOrDefault() {
-        return new Sorter(new Sort.Order(getSortDirection(), getSortBy()));
+        assert sortBy != null;
+        return new Sorter(new Sort.Order(getSortDirection(), sortBy));
     }
 
     public Sorter toSorterOrDefault(String defaultKey) {

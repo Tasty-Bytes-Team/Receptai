1e9bb335d477bfe91ee8a73ac2b702cb753c4cf0
Erikas Laužadis <elauzadis@gmail.com>
2024-05-15 22:48:27 +0300

    Create FeaturedBanner with text

---
+++ Receptai.client/components/HomePage/FeaturedBanner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative w-full h-80 shadow-md">
+    <div class="z-50 rounded-md absolute top-0 left-0 w-full h-full bg-[#00000094] flex justify-center items-center">
+        <div class="flex justify-center items-center flex-col gap-2 mx-10 my-5">
+            <h1 class="text-white font-semibold text-5xl">Tasty Bytes</h1>
+            <p class="text-white text-center leading-5">
+                Craving delicious meals but short on time? Look no further than Tasty Bytes! We offer a mouthwatering collection of recipes designed for busy home cooks. From creative vegetarian options to hearty classics, our recipes are easy to follow and use everyday ingredients. Get ready to spice up your life with Tasty Bytes!
+            </p>
+        </div>
+    </div>
+    <div
+      class="absolute top-0 left-0 rounded-md w-full h-full bg-gray-500 border-black border-2 bg-cover bg-center bg-[url('https://res.cloudinary.com/peloton-cycle/image/fetch/f_auto,c_limit,w_3840,q_90/https://images.ctfassets.net/6ilvqec50fal/4dUbnYDaqf2jRuED5WsMSc/4390b51bdc6b0673b37d09abbec3bad6/longevity-foods.jpg')]"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

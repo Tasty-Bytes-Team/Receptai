96af0994f0e6cecf6490f01a34128d0a5df6b3ee
Erikas Laužadis <elauzadis@gmail.com>
2024-05-16 22:52:57 +0300

    Make category suggestion to engage api, be static

--- Receptai.client/components/HomePage/CategorySuggestion.vue
+++ Receptai.client/components/HomePage/CategorySuggestion.vue
@@ -1,4 +1,5 @@
 <template>
+    <RouterLink :to="`/recipe-category/${recipeID}`">
   <div
     class="bg-gradient-to-br from-whiskey-100 to-whiskey-50 rounded-md p-4 shadow-md border"
   >
@@ -6,14 +7,10 @@
     <div class="flex flex-row flex-wrap items-center gap-[6%]">
       <div class="sm:basis-[47%] basis-full">
         <NuxtImg
-          v-if="category?.previewImageUrl"
           :src="
-            !error
-              ? category?.previewImageUrl
-              : '/assets/TastyBytes_Fallback.webp'
+              url
           "
           class="m-auto rounded-md lg:h-56 h-48 object-cover aspect-[4/3] w-full bg-concrete-200"
-          @error="() => (error = true)"
         />
       </div>
       <div class="sm:basis-[47%] basis-full">
@@ -23,39 +20,16 @@
       </div>
     </div>
   </div>
+</RouterLink>
 </template>
 
 <script setup lang="ts">
-import axios from "axios";
-import type { Category } from "@/typescript/types";
-
-const config = useRuntimeConfig();
-
-const props = defineProps<{
-    id: string;
+defineProps<{
+    recipeID: string;
+    url: string;
     heading: string;
     description: string;
 }>();
-
-const category = ref<Category | null>(null);
-const loading = ref(true);
-const error = ref(false);
-
-const getCategory = async () => {
-  loading.value = true;
-  try {
-    await axios
-      .get(`${config.public.baseURL}/api/v1/category/${props.id}`)
-      .then((res) => {
-        category.value = res.data;
-        loading.value = false;
-      });
-  } catch (e) {
-    console.warn("Error fetching category", e);
-  }
-};
-
-getCategory();
 </script>
 
 <style scoped></style>

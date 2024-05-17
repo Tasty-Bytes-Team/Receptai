8efbe171d83affef368d2cde8c5d7ba9fcdbf9f9
Erikas Laužadis <elauzadis@gmail.com>
2024-05-17 22:31:14 +0300

    Add create tag button

--- Receptai.client/pages/user/admin/dashboard/all-tags/index.vue
+++ Receptai.client/pages/user/admin/dashboard/all-tags/index.vue
@@ -1,13 +1,20 @@
 <template>
   <h1 class="text-3xl font-bold text-center m-3">All Tags</h1>
+  <div class="text-center mb-2">
+    <RouterLink to="/user/admin/dashboard/all-tags/create">
+      <button
+        class="p-1.5 px-5 text-lg rounded-sm text-black font-medium bg-chilean-heath-200 hover:bg-chilean-heath-300 transition-colors duration-200"
+      >
+        Create tag
+      </button>
+    </RouterLink>
+  </div>
   <div>
     <TagsContainer />
   </div>
 </template>
 
 <script setup lang="ts">
-import axios from "axios";
-import type { UserCookie, Tag } from "@/typescript/types";
 import TagsContainer from "@/components/admin/AllTags/TagsContainer.vue";
 
 definePageMeta({

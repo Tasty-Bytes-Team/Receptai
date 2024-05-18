2b7263f8dc3b308dd2d0e09a1da2bd4dc335568b
Erikas Laužadis <45915900+ErikasLau@users.noreply.github.com>
2024-05-18 11:59:29 +0300

    Include user/admin/dashboard data in to the website (#31)
    * Create a featured numbers component, add it to admin dashboard, engage api call to fetch information
    
    * styling update
    
    * Little styling update on user dashboard, chage layouts

--- Receptai.client/pages/user/dashboard/index.vue
+++ Receptai.client/pages/user/dashboard/index.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-
-import type { UserCookie, Recipe } from '@/typescript/types';
-import axios from 'axios';
+import type { UserCookie, Recipe } from "@/typescript/types";
+import axios from "axios";
 import Widget from "./Widgets/WidgetComponent.vue";
-import RecipeBannerComponent from '~/components/Banner/RecipeBannerComponent.vue';
+import RecipeBannerComponent from "~/components/Banner/RecipeBannerComponent.vue";
 
 definePageMeta({
-  layout: "admin",
-  middleware: "auth",
-});
-
-definePageMeta({
-  layout: "admin",
+  layout: "user",
   middleware: "auth",
 });
 
@@ -24,13 +18,15 @@ const stopwatch = ref({
 
 function startStopwatch() {
   if (!stopwatch.value.isRunning) {
-    stopwatch.value.startTime = new Date().getTime() - stopwatch.value.elapsedTime;
+    stopwatch.value.startTime =
+      new Date().getTime() - stopwatch.value.elapsedTime;
     stopwatch.value.isRunning = true;
     stopwatch.value.intervalId = setInterval(updateStopwatch, 10);
   }
 }
 function stopStopwatch() {
-  stopwatch.value.elapsedTime = new Date().getTime() - stopwatch.value.startTime;
+  stopwatch.value.elapsedTime =
+    new Date().getTime() - stopwatch.value.startTime;
   stopwatch.value.isRunning = false;
   stopwatch.value.intervalId && clearInterval(stopwatch.value.intervalId);
   stopwatch.value.intervalId = null;
@@ -51,29 +47,39 @@ function formatTime(time: number) {
   const seconds = Math.floor((time / 1000) % 60);
   const minutes = Math.floor((time / (1000 * 60)) % 60);
   const hours = Math.floor(time / (1000 * 60 * 60));
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
 const config = useRuntimeConfig();
-const TastyBytes_user = useCookie<UserCookie>('TastyBytes_user');
+const TastyBytes_user = useCookie<UserCookie>("TastyBytes_user");
 const recipeList = ref<Recipe[]>([]);
 
 async function fetchRecipes() {
   if (!TastyBytes_user.value?.token) {
-    console.error('User is not logged in or no token available');
+    console.error("User is not logged in or no token available");
     return;
   }
 
   try {
-    const response = await axios.get(`${config.public.baseURL}/api/v1/recipe/list?page=0&sortBy=dateCreated&sortAsc=true`, {
-      headers: {
-        Authorization: `Bearer ${TastyBytes_user.value.token}`
+    const response = await axios.get(
+      `${config.public.baseURL}/api/v1/recipe/list?page=0&sortBy=dateCreated&sortAsc=true`,
+      {
+        headers: {
+          Authorization: `Bearer ${TastyBytes_user.value.token}`,
+        },
       }
-    });
-    
-    const userRecipes = response.data.elements.filter((recipe: Recipe) => recipe.author.id === TastyBytes_user.value.user.id);
-    
-    userRecipes.sort((a: Recipe, b: Recipe) => new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime());
+    );
+
+    const userRecipes = response.data.elements.filter(
+      (recipe: Recipe) => recipe.author.id === TastyBytes_user.value.user.id
+    );
+
+    userRecipes.sort(
+      (a: Recipe, b: Recipe) =>
+        new Date(b.dateCreated).getTime() - new Date(a.dateCreated).getTime()
+    );
 
     recipeList.value = userRecipes;
   } catch (error) {
@@ -82,23 +88,28 @@ async function fetchRecipes() {
 }
 
 onMounted(fetchRecipes);
-
 </script>
 
 <template>
-  <div class="bg-gray-100 py-12 min-h-screen">
+  <div>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="mb-8">
+      <div class="mb-4 p-2 border-b-2">
         <h1 class="text-4xl font-bold text-gray-800">Dashboard</h1>
-        <p class="text-lg text-gray-600 mt-2">
-          Welcome back, {{ TastyBytes_user.user.name }}!
+        <p class="text-lg text-gray-600">
+          Welcome back, {{ TastyBytes_user?.user.name }}!
         </p>
       </div>
 
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         <div class="col-span-1 sm:col-span-2 lg:col-span-2">
-          <div class="bg-white rounded-lg shadow-md p-6 flex items-center justify-center" >
-            <Widget class="border" orientation="horizontal" title="Your recipe count is">
+          <div
+            class="bg-white rounded-lg shadow-md p-6 flex items-center justify-center"
+          >
+            <Widget
+              class="border"
+              orientation="horizontal"
+              title="Your recipe count is"
+            >
               <div class="flex items-center justify-center">
                 {{ recipeList.length }}
               </div>
@@ -124,9 +135,7 @@ onMounted(fetchRecipes);
                     />
                   </div>
                 </template>
-                <div v-else>
-                  No recent recipes found or still loading...
-                </div>
+                <div v-else>No recent recipes found or still loading...</div>
               </div>
             </Widget>
           </div>

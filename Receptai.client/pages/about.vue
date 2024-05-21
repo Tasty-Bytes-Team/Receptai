cc620f841016b0443917b79adeecdc0c648ac14b
Issiins <160136271+Issiins@users.noreply.github.com>
2024-05-21 15:07:17 +0300

    Updated about page
    Filled 'about' page with information

--- Receptai.client/pages/about.vue
+++ Receptai.client/pages/about.vue
@@ -1,5 +1,50 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>About</div>
+  <div class="max-w-4xl mx-auto px-4 py-8">
+    <div class="mb-8">
+      <h1 class="text-3xl font-bold mb-4">About Us</h1>
+      <p>
+        Welcome to our recipe website! Our mission is to inspire and empower home cooks by providing 
+        a wide range of delicious and easy-to-follow recipes. Developed as a university project, this website was created by Erikas L., Dominykas S., and Dovydas M. 
+        we are passionate about making cooking accessible and enjoyable for everyone.
+      </p>
+    </div>
+
+    <div class="mb-8">
+      <h2 class="text-2xl font-bold mb-4">Our Mission</h2>
+      <p>
+        Our mission is to provide a comprehensive resource for home cooks of all skill levels. 
+        We aim to offer recipes that are not only tasty and nutritious but also simple to prepare. 
+        Whether you're a beginner looking to learn the basics or an experienced cook seeking new inspiration, 
+        our goal is to make your culinary journey enjoyable and rewarding.
+      </p>
+    </div>
+
+    <div class="mb-8">
+      <h2 class="text-2xl font-bold mb-4">Website Functions</h2>
+      <ul class="list-disc pl-6">
+        <li>Create and share your own recipes</li>
+        <li>Leave reviews and ratings for recipes</li>
+        <li>Find recipes by categories such as cuisine type, dietary preferences, or occasion</li>
+        <li>Utilize a stopwatch feature for making recipes with precise timing</li>
+        <li>Print recipes for easy reference in the kitchen</li>
+      </ul>
+    </div>
+
+    <div>
+      <h2 class="text-2xl font-bold mb-4">What You'll Find</h2>
+      <p>
+        On our website, you can expect to find a diverse collection of recipes, including:
+      </p>
+      <ul class="list-disc pl-6 mt-2">
+        <li>Quick and Easy Weeknight Dinners</li>
+        <li>Healthy and Nutritious Meals</li>
+        <li>Gourmet Dishes for Special Occasions</li>
+        <li>Vegan and Vegetarian Options</li>
+        <li>Decadent Desserts</li>
+        <li>Cooking Tips and Techniques</li>
+      </ul>
+    </div>
+  </div>
 </template>
